14f66b26661faf94c5fbee4899179f0f20386e6b
Rx Chris Ameh <chris1jnr@gmail.com>
2024-09-29 11:18:40 -0700

    main.c

--- main.c
+++ main.c
@@ -1,8 +1,9 @@
 #include "monty.h"
+
 bus_t bus = {NULL, NULL, NULL, 0};
 
 /**
- * getline - Reads an entire line from stream.
+ * getline - Reads an entire line from a stream.
  * @lineptr: Pointer to the buffer containing the line read.
  * @n: Pointer to the size of the buffer.
  * @stream: The stream to read from.
@@ -11,89 +12,88 @@ bus_t bus = {NULL, NULL, NULL, 0};
  */
 ssize_t getline(char **lineptr, size_t *n, FILE *stream)
 {
-    char *buffer;
-    int c;
-    size_t len = 0;
+	char *buffer;
+	int c;
+	size_t len = 0;
 
-    if (!lineptr || !n || !stream)
-        return -1;
+	if (!lineptr || !n || !stream)
+		return (-1);
 
-    if (*lineptr == NULL)
-    {
-        *n = 128;
-        *lineptr = malloc(*n);
-        if (!*lineptr)
-            return -1;
-    }
+	if (*lineptr == NULL)
+	{
+		*n = 128;
+		*lineptr = malloc(*n);
+		if (!*lineptr)
+			return (-1);
+	}
 
-    buffer = *lineptr;
+	buffer = *lineptr;
 
-    while ((c = fgetc(stream)) != EOF)
-    {
-        if (len + 1 >= *n)
-        {
-            *n *= 2;
-            buffer = realloc(buffer, *n);
-            if (!buffer)
-                return -1;
-            *lineptr = buffer;
-        }
-        buffer[len++] = c;
-        if (c == '\n')
-            break;
-    }
+	while ((c = fgetc(stream)) != EOF)
+	{
+		if (len + 1 >= *n)
+		{
+			*n *= 2;
+			buffer = realloc(buffer, *n);
+			if (!buffer)
+				return (-1);
+			*lineptr = buffer;
+		}
+		buffer[len++] = c;
+		if (c == '\n')
+			break;
+	}
 
-    if (len == 0)
-        return -1;
+	if (len == 0)
+		return (-1);
 
-    buffer[len] = '\0';
-    return len;
+	buffer[len] = '\0';
+	return (len);
 }
 
 /**
-* main - monty code interpreter
-* @argc: number of arguments
-* @argv: monty file location
-* Return: 0 on success
-*/
+ * main - Monty code interpreter.
+ * @argc: Number of arguments.
+ * @argv: Monty file location.
+ *
+ * Return: 0 on success.
+ */
 int main(int argc, char *argv[])
 {
-    char *content;
-    FILE *file;
-    size_t size = 0;
-    ssize_t read_line = 1;
-    stack_t *stack = NULL;
-    unsigned int counter = 0;
+	char *content;
+	FILE *file;
+	size_t size = 0;
+	ssize_t read_line = 1;
+	stack_t *stack = NULL;
+	unsigned int counter = 0;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(EXIT_FAILURE);
-    }
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
 
-    file = fopen(argv[1], "r");
-    bus.file = file;
-    if (!file)
-    {
-        fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-        exit(EXIT_FAILURE);
-    }
+	file = fopen(argv[1], "r");
+	bus.file = file;
+	if (!file)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
 
-    while (read_line > 0)
-    {
-        content = NULL;
-        read_line = getline(&content, &size, file);
-        bus.content = content;
-        counter++;
-        if (read_line > 0)
-        {
-            execute(content, &stack, counter, file);
-        }
-        free(content);
-    }
+	while (read_line > 0)
+	{
+		content = NULL;
+		read_line = getline(&content, &size, file);
+		bus.content = content;
+		counter++;
+		if (read_line > 0)
+			execute(content, &stack, counter, file);
+		free(content);
+	}
 
-    free_stack(stack);
-    fclose(file);
-    return (0);
+	free_stack(stack);
+	fclose(file);
+	return (0);
 }
 
